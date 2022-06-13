71bc79e71d0e5cbafa6e2e3de2750134823e7f81
BakulinCorp <87490884+BakulinCorp@users.noreply.github.com>
2022-06-13 17:44:22 +0300

    Finished Application v1.0

--- BullseyeTests/BullseyeTests.swift
+++ BullseyeTests/BullseyeTests.swift
@@ -9,12 +9,12 @@ import XCTest
 @testable import Bullseye
 
 class BullseyeTests: XCTestCase {
-    
+
     var game: Game!
 
     override func setUpWithError() throws {
         game = Game()
-    
+
     }
 
     override func tearDownWithError() throws {
@@ -27,26 +27,26 @@ class BullseyeTests: XCTestCase {
         XCTAssertEqual(score, 95)
     }
 
-    
+
     func testScoreNegative() {
         let guess = game.target - 5
         let score = game.points(sliderValue: guess)
         XCTAssertEqual(score, 95)
     }
-    
+
     func testNewRound() {
-        
+
         game.startNewRound(points: 100)
         XCTAssertEqual(game.score, 100)
         XCTAssertEqual(game.round, 2)
     }
-    
+
     func testScoreExact() {
         let guess = game.target
         let score = game.points(sliderValue: guess)
         XCTAssertEqual(score, 200)
     }
-    
+
     func testScoreClose() {
         let guess = game.target + 2
         let score = game.points(sliderValue: guess)
@@ -54,14 +54,14 @@ class BullseyeTests: XCTestCase {
     }
 
     func testRestart() {
-        
+
         game.startNewRound(points: 100)
         XCTAssertNotEqual(game.score, 0)
         XCTAssertNotEqual(game.round, 1)
         game.restart()
         XCTAssertEqual(game.score, 0)
         XCTAssertEqual(game.round, 1)
-        
+
     }
 
     func testLeaderboard() {
